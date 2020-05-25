fde034379339b4465cfc0ee0aa59110ca418b539
Gabriel Reyes <ghreyes921@gmail.com>
2020-05-25 15:11:26 -0500

    Update log functionality

---
+++ ScrumManager/Controllers/LogController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScrumManager.Models;
+using ScrumManager.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace ScrumManager.Controllers
+{
+    [Route("[controller]")]
+    public class LogController : Controller
+    {
+        LogService _logService;
+
+        public LogController()
+        {
+            _logService = new LogService();
+        }
+
+        [HttpPost("UpdateLog")]
+        public async Task<IActionResult> UpdateLog(Log log)
+        {
+            var f = Request.Form;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _logService.CreateOrUpdate(log);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ ScrumManager/Services/LogService.cs
@@ -0,0 +1,25 @@
+﻿using Google.Cloud.Firestore;
+using ScrumManager.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace ScrumManager.Services
+{
+    public class LogService
+    {
+        public async Task CreateOrUpdate(Log log)
+        {
+            string credential_path = @"C:\Users\ghrey\Downloads\ScrumManager-c7ce2bf2810c.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credential_path);
+
+            FirestoreDb db = FirestoreDb.Create("scrummanager");
+            WriteBatch batch = db.StartBatch();
+            var logDoc = db.Collection("logs").Document(log.DocId);
+            //batch.Create(logDoc, log);
+
+            batch.Set(logDoc, log);
+
+            var batchResult = await batch.CommitAsync();
+        }
+    }
+}
