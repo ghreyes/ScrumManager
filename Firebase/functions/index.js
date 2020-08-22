16d95e30749b193396f9ad75d5f2b73d18d45d6e
Gabriel Reyes <ghreyes921@gmail.com>
2020-08-22 01:05:37 -0500

    Cloud functions to handle user-group relationship

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,4 +1,4 @@
-// open firebase-tools app
+ // open firebase-tools app
 // navigate to current folder
 // 'firebase init functions' - create base files
 // 'firebase deploy --only functions' - deploy any cloud functions
@@ -15,6 +15,92 @@ const db = admin.firestore();
 // Function to delete any users that are deleted in Firebase
 exports.deleteUser = functions.auth.user().onDelete((user) => {
     db.doc('users/' + user.uid).delete();
+});
+
+// Function to remove deleted users from groups
+exports.userDeleted_Groups = functions.firestore.document('users/{userId}').onDelete((user, context) => {
+    //DEBUG ONLY
+    //db.doc('users/DeleteMe').set(user.data())
+    //    .then(data => {
+    //        functions.logger.log('Clone Success');
+    //        return;
+    //    })
+    //    .catch(error => {
+    //        console.error('Clone Failure - ', error);
+    //        throw error;
+    //    });
+
+    var docId = context.params.userId;
+    Object.keys(user.data().Groups).forEach(g => {
+        try {
+            functions.logger.log('Deleting user {' + docId + '} from group {' + g + '}');
+            db.collection('groups').doc(g).update({ ['Users.' + docId]: admin.firestore.FieldValue.delete() });
+            functions.logger.log('Deleted user {' + docId + '} from group {' + g + '}');
+        }
+        catch (ex) {
+            functions.logger.error(ex.message);
+        }
+    });
+    return 0;
+});
+
+// Function to update users in groups
+exports.userUpdated_Groups = functions.firestore.document('users/{userId}').onUpdate((user, context) => {
+    var docId = context.params.userId;
+    Object.keys(user.after.data().Groups).forEach(g => {
+        try {
+            functions.logger.log('Updating user {' + docId + '} in group {' + g + '}');
+            db.collection('groups').doc(g).update({ ['Users.' + docId + '.DisplayName']: user.after.data().DisplayName });
+            functions.logger.log('Updated user {' + docId + '} in group {' + g + '}');
+        }
+        catch (ex) {
+            functions.logger.error(ex.message);
+        }
+    });
+    return 0;
+});
+
+// Function to remove deleted groups from users
+exports.groupDeleted_Users = functions.firestore.document('groups/{groupId}').onDelete((group, context) => {
+    //DEBUG ONLY
+    //db.doc('groups/DeleteMe').set(group.data())
+    //    .then(data => {
+    //        functions.logger.log('Clone Success');
+    //        return;
+    //    })
+    //    .catch(error => {
+    //        console.error('Clone Failure - ', error);
+    //        throw error;
+    //    });
+
+    var docId = context.params.groupId;
+    Object.keys(group.data().Users).forEach(u => {
+        try {
+            functions.logger.log('Deleting group {' + docId + '} from user {' + u + '}');
+            db.collection('users').doc(u).update({ ['Groups.' + docId]: admin.firestore.FieldValue.delete() });
+            functions.logger.log('Deleted group {' + docId + '} from user {' + u + '}');
+        }
+        catch (ex) {
+            functions.logger.error(ex.message);
+        }
+    });
+    return 0;
+});
+
+// Function to update groups in users
+exports.groupUpdated_Users = functions.firestore.document('groups/{groupId}').onUpdate((group, context) => {
+    var docId = context.params.groupId;
+    Object.keys(group.after.data().Users).forEach(u => {
+        try {
+            functions.logger.log('Updating group {' + docId + '} in user {' + u + '}');
+            db.collection('users').doc(u).update({ ['Groups.' + docId + '.Name']: group.after.data().Data.Name });
+            functions.logger.log('Updated group {' + docId + '} in user {' + u + '}');
+        }
+        catch (ex) {
+            functions.logger.error(ex.message);
+        }
+    });
+    return 0;
 });
 
 // Weekly PDF Job
